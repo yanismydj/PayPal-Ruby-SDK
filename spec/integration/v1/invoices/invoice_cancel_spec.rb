af6e876b1f08a1767e7d2479f5edd161fcdafdd4
sdcoffey <steve.coffey@getbraintree.com>
2018-02-01 10:22:15 -0800

    Add invoice cancel spec

--- spec/integration/v1/invoices/invoice_cancel_spec.rb
+++ spec/integration/v1/invoices/invoice_cancel_spec.rb
@@ -1,23 +1,23 @@
-
-
 require_relative '../../test_harness'
-require_relative '../../../lib/lib'
+require_relative './invoice_helper'
 require 'json'
 
 include PayPal::V1::Invoices
 
 describe InvoiceCancelRequest do
-  def build_request_body
-    return JSON.parse('{"cc_emails":["D1tbwWfJDf ","3ZdrXqE1 JD3"],"note":"TPNXIhDUqPCZuUKpOz","send_to_merchant":true,"send_to_payer":true,"subject":"EMFwSsVAbsXDdp0Z"}')
-  end
-
   it 'successfully makes a request' do
-    request = InvoiceCancelRequest.new("CMavupaQbed85WwK")
-    request.request_body(build_request_body)
+    create_response = InvoiceHelper::create_invoice
+    InvoiceHelper::send_invoice(create_response.result.id)
+
+    request = InvoiceCancelRequest.new(create_response.result.id)
+    request.request_body({
+      :subject => 'Past Due',
+      :note => 'nevermind',
+      :send_to_merchant => true,
+      :send_to_payer => true
+    })
 
     resp = TestHarness::client.execute(request)
-    expect(resp.status_code).to eq(200)
-
-    # Add your own checks here
+    expect(resp.status_code).to eq(204)
   end
 end
